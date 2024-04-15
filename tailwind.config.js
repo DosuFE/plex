ed357b959310d01d8870ef932f0a2bc561af6e29
DosuFE <sdosu@dervac.com>
2024-04-15 10:08:02 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { 
+      backgroundImage: {
+        'heropattern': "url('./images/movies-shows-bg.png')",
+        'blog-hero': "url('./images/rental-blog-hero.png')",
+        'plex-hero': "url('./images/plex_hero_eoy.png')",
+        'community-phase': "url('./images/Hero-Community-Phase.png')",
+        'about-header': "url('./images/hero-about-living-room (2).png')",
+        'section-bg': "url('./images/mission-image-new.jpg')",
+        'vision-bg': "url('./images/vision-bg-image.png')",
+        'about-pattern': "url('./images/about-partner-bg-image.png')",
+      }
+ },
+  },
+  plugins: [],
+}
